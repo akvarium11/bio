4fc1f5b09ab099ac8ea7b1aa49c9b02c8c018606
akvarium11 <akvarium11@github.com>
2026-07-04 03:21:08 -0700

    song order

--- public/script.js
+++ public/script.js
@@ -161,16 +161,16 @@ document.addEventListener('DOMContentLoaded', () => {
     // Убедитесь, что файлы лежат в папке u:\web\bio\assets\songs\
     const tracks = [
         {
-            title: "Illusionary Night [Fuwa Fuwa Spring Storm]", // То, что будет написано в плеере
-            src: "assets/songs/ELECTR, Fuwa Fuwa Spring Storm - Illusionary Night.mp3", // Путь к локальному файлу
-            cover: "https://i1.sndcdn.com/artworks-1ZyMjlvONjI9vzw0-0MBvuw-t500x500.jpg", // Ссылка на обложку (или локальный путь /assets/cover1.jpg)
-            url: "https://soundcloud.com/electr-nics/illusionary-night" // Ссылка, которая откроется при клике на название
-        },
-        {
             title: "Toromi hearts 2", 
             src: "/assets/songs/goreshit - toromi hearts 2.mp3",  
             cover: "assets/cover.jpg", 
             url: "https://soundcloud.com/goreshit/toromi-hearts-2"
+        },
+        {
+            title: "Illusionary Night [Fuwa Fuwa Spring Storm]", // То, что будет написано в плеере
+            src: "assets/songs/ELECTR, Fuwa Fuwa Spring Storm - Illusionary Night.mp3", // Путь к локальному файлу
+            cover: "https://i1.sndcdn.com/artworks-1ZyMjlvONjI9vzw0-0MBvuw-t500x500.jpg", // Ссылка на обложку (или локальный путь /assets/cover1.jpg)
+            url: "https://soundcloud.com/electr-nics/illusionary-night" // Ссылка, которая откроется при клике на название
         },
         {
             title: "Miss The Rage (Sewerslvt Remix)",
